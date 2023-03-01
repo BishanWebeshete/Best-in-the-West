53b064e4a8ec6f991d69a84ddd20349aa3f1b917
Bishan Webeshete <webesheteb@gmail.com>
2023-03-01 19:51:16 +0000

    updated table rendering function to remove prior table once a new team has been clicked

--- js/main.js
+++ js/main.js
@@ -1,28 +1,38 @@
 var $ol = document.querySelector('ol');
+
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://statsapi.web.nhl.com/api/v1/standings?season=20222023');
 xhr.responseType = 'json';
 xhr.addEventListener('load', function () {
-  // console.log(xhr.response);
   for (let i = 0; i < xhr.response.records[3].teamRecords.length; i++) {
+    var teamID = xhr.response.records[3].teamRecords[i].team.id;
+    var teamName = xhr.response.records[3].teamRecords[i].team.name;
     var $li = document.createElement('li');
     var $img = document.createElement('img');
-    if (xhr.response.records[3].teamRecords[i].team.name === 'Vegas Golden Knights') {
+    if (teamName === 'Vegas Golden Knights') {
       $img.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/en/thumb/a/ac/Vegas_Golden_Knights_logo.svg/800px-Vegas_Golden_Knights_logo.svg.png');
-    } else if (xhr.response.records[3].teamRecords[i].team.name === 'Los Angeles Kings') {
+      $img.setAttribute('id', teamID);
+    } else if (teamName === 'Los Angeles Kings') {
       $img.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/en/thumb/6/63/Los_Angeles_Kings_logo.svg/1200px-Los_Angeles_Kings_logo.svg.png');
-    } else if (xhr.response.records[3].teamRecords[i].team.name === 'Edmonton Oilers') {
+      $img.setAttribute('id', teamID);
+    } else if (teamName === 'Edmonton Oilers') {
       $img.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4d/Logo_Edmonton_Oilers.svg/1200px-Logo_Edmonton_Oilers.svg.png');
-    } else if (xhr.response.records[3].teamRecords[i].team.name === 'Seattle Kraken') {
+      $img.setAttribute('id', teamID);
+    } else if (teamName === 'Seattle Kraken') {
       $img.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/en/thumb/4/48/Seattle_Kraken_official_logo.svg/1200px-Seattle_Kraken_official_logo.svg.png');
-    } else if (xhr.response.records[3].teamRecords[i].team.name === 'Calgary Flames') {
+      $img.setAttribute('id', teamID);
+    } else if (teamName === 'Calgary Flames') {
       $img.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/en/thumb/6/61/Calgary_Flames_logo.svg/1200px-Calgary_Flames_logo.svg.png');
-    } else if (xhr.response.records[3].teamRecords[i].team.name === 'Vancouver Canucks') {
+      $img.setAttribute('id', teamID);
+    } else if (teamName === 'Vancouver Canucks') {
       $img.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3a/Vancouver_Canucks_logo.svg/1200px-Vancouver_Canucks_logo.svg.png');
-    } else if (xhr.response.records[3].teamRecords[i].team.name === 'San Jose Sharks') {
+      $img.setAttribute('id', teamID);
+    } else if (teamName === 'San Jose Sharks') {
       $img.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/en/thumb/3/37/SanJoseSharksLogo.svg/1200px-SanJoseSharksLogo.svg.png');
-    } else if (xhr.response.records[3].teamRecords[i].team.name === 'Anaheim Ducks') {
+      $img.setAttribute('id', teamID);
+    } else if (teamName === 'Anaheim Ducks') {
       $img.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/en/thumb/7/72/Anaheim_Ducks.svg/1200px-Anaheim_Ducks.svg.png');
+      $img.setAttribute('id', teamID);
     }
     $li.appendChild($img);
     $ol.appendChild($li);
@@ -30,26 +40,37 @@ xhr.addEventListener('load', function () {
 });
 xhr.send();
 
-var xhr1 = new XMLHttpRequest();
-xhr1.open('GET', 'https://statsapi.web.nhl.com/api/v1/teams/54/roster?season=20222023');
-xhr1.responseType = 'json';
-xhr1.addEventListener('load', function () {
-  // console.log(xhr1.status);
-  // console.log(xhr1.response);
-  // console.log(xhr1.response.roster[0].person.fullName);
-  var $tbody = document.querySelector('tbody');
-  for (let i = 0; i < xhr1.response.roster.length; i++) {
-    var $tr = document.createElement('tr');
-    var $th1 = document.createElement('th');
-    $th1.textContent = xhr1.response.roster[i].jerseyNumber;
-    var $th2 = document.createElement('th');
-    $th2.textContent = xhr1.response.roster[i].person.fullName;
-    var $th3 = document.createElement('th');
-    $th3.textContent = xhr1.response.roster[i].position.abbreviation;
-    $tr.appendChild($th1);
-    $tr.appendChild($th2);
-    $tr.appendChild($th3);
-    $tbody.appendChild($tr);
+var $imagesContainer = document.querySelector('.listed-images-container');
+$imagesContainer.addEventListener('click', function (event) {
+  if (event.target.tagName !== 'IMG') {
+    return;
   }
+  var teamID = event.target.id;
+  var xhr1 = new XMLHttpRequest();
+  xhr1.open('GET', 'https://statsapi.web.nhl.com/api/v1/teams/' + teamID + '/roster?season=20222023');
+  xhr1.responseType = 'json';
+  xhr1.addEventListener('load', function () {
+    var $body = document.querySelector('tbody');
+    if ($body) {
+      $body.remove();
+    }
+    var $tbody = document.createElement('tbody');
+    for (let i = 0; i < xhr1.response.roster.length; i++) {
+      var $table = document.querySelector('table');
+      var $tr = document.createElement('tr');
+      var $td1 = document.createElement('td');
+      $td1.textContent = xhr1.response.roster[i].jerseyNumber;
+      var $td2 = document.createElement('td');
+      $td2.textContent = xhr1.response.roster[i].person.fullName;
+      var $td3 = document.createElement('td');
+      $td3.textContent = xhr1.response.roster[i].position.abbreviation;
+      $tr.appendChild($td1);
+      $tr.appendChild($td2);
+      $tr.appendChild($td3);
+      $tbody.appendChild($tr);
+      $table.appendChild($tbody);
+    }
+
+  });
+  xhr1.send();
 });
-xhr1.send();
